81b8cc10aa8b3e3f69e3a3c49eae7271463a1809
phu duc <ducnphe161175@fpt.edu.vn>
2025-03-23 10:45:44 +0700

    1. Dish display adapter . user_feed_back display
    1. Dish display adapter . user_feed_back display

---
+++ app/src/main/java/com/example/foodorderapp/Activity/DishDisplayAdapter.java
@@ -0,0 +1,78 @@
+package com.example.foodorderapp.Activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.foodorderapp.Entity.Dish;
+import com.example.foodorderapp.R;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.List;
+
+public class DishDisplayAdapter extends RecyclerView.Adapter<DishDisplayAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Dish> listFood;
+    DatabaseReference databaseReference;
+    public DishDisplayAdapter(Context context, List<Dish> listFood){
+        this.context=context;
+        this.listFood = listFood;
+
+    }
+    @NonNull
+    @Override
+    public DishDisplayAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_dish_menu_display,parent,false);
+        return new DishDisplayAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DishDisplayAdapter.ViewHolder holder, int position) {
+        final Dish food = listFood.get(position);
+        String imagePath = "android.resource://" + context.getPackageName() + "/drawable/"+food.getImageUrl();
+        Glide.with(context)
+                .load(Uri.parse(imagePath))
+                .into(holder.imageView);
+        holder.FoodName.setText(food.getName());
+        holder.FoodPrice.setText("Price: "+food.getPrice()+" dong ");
+        // Xử lý sự kiện click vào CardView
+
+//        holder.imageView.setOnClickListener(v -> {
+//            Intent intent = new Intent(context, FoodDetailActivity.class);
+//
+//            // Truyền dữ liệu qua Intent
+//            intent.putExtra("DishId", food.getDishId());
+//            context.startActivity(intent);
+//        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listFood.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+        TextView FoodName,FoodPrice;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView=itemView.findViewById(R.id.Food_Image);
+            FoodName=itemView.findViewById(R.id.Food_Name);
+            FoodPrice=itemView.findViewById(R.id.Food_Price);
+
+        }
+    }
+
+}
